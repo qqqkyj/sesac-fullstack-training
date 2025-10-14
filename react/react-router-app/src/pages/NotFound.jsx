10abf64b65441fc01a7c6ed04241a86a6756e654
qqqkyj <123rkdduswn@naver.com>
2025-10-14 16:22:03 +0900

    not found

--- react/react-router-app/src/pages/NotFound.jsx
+++ react/react-router-app/src/pages/NotFound.jsx
@@ -6,7 +6,8 @@ import { Link } from "react-router-dom";
 export default function NotFound() {
 	return (
 		<div>
-			404 Error!! 페이지를 찾을 수 없습니다.<br></br>
+      404 Error!! 페이지를 찾을 수 없습니다.<br>
+      </br>
 			<Link className="border-2" to={PATHS.ROOT.INDEX}>
 				홈페이지로 이동
 			</Link>
