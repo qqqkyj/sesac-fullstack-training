cdc757b99f6f62658c0249c318b7749f805bb7db
qqqkyj <123rkdduswn@naver.com>
2025-11-17 14:22:56 +0900

    springboot - PathVariable

--- springboot/firstapp/src/main/java/com/example/firstapp/controller/HomeController.java
+++ springboot/firstapp/src/main/java/com/example/firstapp/controller/HomeController.java
@@ -3,9 +3,13 @@ package com.example.firstapp.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.time.Year;
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 @Controller
 public class HomeController {
@@ -49,5 +53,57 @@ public class HomeController {
         int score = 90;
         model.addAttribute("score", score);
         return "grade";
+    }
+
+    @GetMapping("/lunch")
+    public String lunch(Model model){
+        List<String> menus = new ArrayList<>(Arrays.asList("김밥", "라면", "돈가스"));
+        model.addAttribute("pick", menus.get(new Random().nextInt(menus.size())));
+        return "lunch";
+    }
+
+    @GetMapping("/lotto")
+    public String lotto(Model model){
+        List<Integer> numbers = IntStream.rangeClosed(1,46)
+                .boxed()//auto boxing(int형태의 데이터를 Integer로 형변환)
+                .collect(Collectors.toList());
+
+        Collections.shuffle(numbers);
+        List<Integer> lucky = numbers.subList(0, 6);
+        model.addAttribute("lucky", lucky);
+        return "lotto";
+    }
+
+    // PathVariable : url을 통해 들어오는 변수(ex.username)
+    @GetMapping("/profile/{username}")
+    public String profile(@PathVariable String username, Model model){
+        model.addAttribute("username", username);
+        return "profile";
+    }
+
+    @GetMapping("/cube/{number}")
+    public String cube(@PathVariable int number, Model model){
+        model.addAttribute("number", number);
+        model.addAttribute("result", number * number * number);
+        return "cube";
+    }
+
+    // 짝수 홀수 판별
+    // /number/{num} => 짝수인지 홀수인지 판별해서 화면에 출력
+    @GetMapping("/number/{num}")
+    public String number(@PathVariable int num, Model model){
+        model.addAttribute("num", num);
+        model.addAttribute("result", num % 2 == 0 ? "짝수" : "홀수");
+        return "number";
+    }
+
+    //나이계산 (Year클래스 사용)
+    // /age/{birthYear} => 현재 나이를 계산해서 출력
+    // /age/1990 => 36살입니다.
+    @GetMapping("/age/{birthYear}")
+    public String age(@PathVariable int birthYear, Model model){
+        model.addAttribute("birthYear", birthYear);
+        model.addAttribute("age", Year.now().getValue() - birthYear + 1);
+        return "age";
     }
 }
