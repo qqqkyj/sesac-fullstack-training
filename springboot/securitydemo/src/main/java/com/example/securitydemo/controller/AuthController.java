f4ac64315bf5cfb36d8e172ddb489bfe813c1d0f
qqqkyj <123rkdduswn@naver.com>
2025-11-28 11:14:48 +0900

    spring security - 사용자정보 출력

--- springboot/securitydemo/src/main/java/com/example/securitydemo/controller/AuthController.java
+++ springboot/securitydemo/src/main/java/com/example/securitydemo/controller/AuthController.java
@@ -1,6 +1,9 @@
 package com.example.securitydemo.controller;
 
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
@@ -12,7 +15,12 @@ public class AuthController {
     }
 
     @GetMapping("/dashboard")
-    public String dashboard(){
+    public String dashboard(@AuthenticationPrincipal UserDetails userDetails, Model model){
+        if(userDetails != null){
+            model.addAttribute("username",userDetails.getUsername());
+            model.addAttribute("password",userDetails.getPassword());
+            model.addAttribute("roles",userDetails.getAuthorities());
+        }
         return "dashboard";
     }
 }
